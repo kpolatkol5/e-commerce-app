b30a6c39a727fa6f1738be9445e8de6b9dbc2686
kadir polatkol <kpolatkol5@gmail.com>
2022-12-13 15:28:48 +0300

    ana uygulamada static dosyalar ve media dosyaları için ayarlamalar yapıldı urls dosyası diğer uygulamalarla bağlandı

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -122,7 +122,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS=[
+    BASE_DIR / "static"
+]
+
+
+
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/images/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
