0e85e13fc95c8b68099b39bdc2bcb62fcc63f59b
kadir polatkol <kpolatkol5@gmail.com>
2022-12-17 21:03:53 +0300

    context_processor dosyası dosyası eklendi settings ve urls dosyası güncellendi

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'mptt',
     'product.apps.ProductConfig',
+    'django_extensions',
 ]
 
 AUTH_USER_MODEL = 'userauth.Kullanicilar'
@@ -71,13 +72,15 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                #custom context_processors 
+                'ecommerce.context_processors.custom_context_processors',
             ],
         },
     },
 ]
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
-
+LOGIN_URL = 'auth/login'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
@@ -120,6 +123,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+MPTT_ADMIN_LEVEL_INDENT = 20
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
